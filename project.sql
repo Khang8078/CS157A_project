708fa234afe1dcd4c9cd18af407eee59ea3a3b3e
Khang Huynh <khanghuynh8078@gmail.com>
2023-11-18 00:15:42 -0800

    project sql

--- project.sql
+++ project.sql
@@ -137,46 +137,49 @@ VALUES
   (4, 4, 'Standard Shipping', 'Ground', '2023-11-12'),
   (5, 5, 'Express Shipping', 'Air', '2023-11-15');
 
-  CREATE TABLE `customer_payment_method` (
+ CREATE TABLE `customer_payment_method` (
   `payment_id` int(11) NOT NULL AUTO_INCREMENT,
   `customer_id` int(11) NOT NULL,
   `type` varchar(50) NOT NULL,
   `provider` varchar(50) NOT NULL,
-  `number` varchar(16) NOT NULL,
+  `number` varchar(20) NOT NULL, -- Change the data type to varchar
   `expires` int(11) NOT NULL,
   `is_default` tinyint(1) NOT NULL,
   PRIMARY KEY (`payment_id`),
   CONSTRAINT `fk_payment_customers` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- Insert data into `customer_payment_method` table
+
 INSERT INTO `customer_payment_method` (`payment_id`, `customer_id`, `type`, `provider`, `number`, `expires`, `is_default`)
 VALUES
-  (1, 1, 'Credit Card', 'Visa', 1234567890123456, 1225, 1),
-  (2, 2, 'PayPal', 'PayPal', 'paypal@example.com', 1125, 0),
-  (3, 3, 'Debit Card', 'MasterCard', 9876543210123456, 0324, 1),
-  (4, 4, 'Credit Card', 'MasterCard', 1234567890123456, 0625, 1),
-  (5, 5, 'PayPal', 'PayPal', 'paypal@example.com', 0924, 0);
+  (1, 1, 'Credit Card', 'Visa', '123456789012345', 1225, 1),
+  (2, 2, 'PayPal', 'PayPal', '2222222222222222', 1125, 0),
+  (3, 3, 'Debit Card', 'MasterCard', '9876543210123456', 0324, 1),
+  (4, 4, 'Credit Card', 'MasterCard', '1234567890123456', 0625, 1),
+  (5, 5, 'PayPal', 'PayPal', '555555555555555', 0924, 0);
 
-
+-- Create the orders table
 CREATE TABLE `orders` (
   `order_id` int(11) NOT NULL AUTO_INCREMENT,
   `customer_id` int(11) NOT NULL,
   `order_date` date NOT NULL,
   `status` varchar(50) NOT NULL,
   `shipping_id` int(11) DEFAULT NULL,
+  `order_total` decimal(10, 2) NOT NULL,
   PRIMARY KEY (`order_id`),
   KEY `fk_orders_customers_idx` (`customer_id`),
   KEY `fk_orders_order_statuses_idx` (`status`),
   CONSTRAINT `fk_orders_customers` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`) ON UPDATE CASCADE,
   CONSTRAINT `fk_orders_shippers` FOREIGN KEY (`shipping_id`) REFERENCES `shipping` (`shipping_id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-INSERT INTO `orders` (`order_id`, `customer_id`, `order_date`, `status`, `shipping_id`)
+
+-- Insert data into the orders table
+INSERT INTO `orders` (`order_id`, `customer_id`, `order_date`, `status`, `shipping_id`, `order_total`)
 VALUES
-  (1, 1, '2023-11-01', 'Processing', 1),
-  (2, 2, '2023-11-03', 'Shipped', 2),
-  (3, 3, '2023-11-06', 'Delivered', 3),
-  (4, 4, '2023-11-10', 'Processing', 4),
-  (5, 5, '2023-11-13', 'Shipped', 5);
+  (1, 1, '2023-11-01', 'Processing', 1, 0),
+  (2, 2, '2023-11-03', 'Shipped', 2, 0),
+  (3, 3, '2023-11-06', 'Delivered', 3, 0),
+  (4, 4, '2023-11-10', 'Processing', 4, 0),
+  (5, 5, '2023-11-13', 'Shipped', 5, 0);
 
 
 CREATE TABLE `order_items` (
@@ -198,6 +201,63 @@ VALUES
   (4, 5, 2, 49.99),
   (5, 6, 1, 69.99),
   (5, 7, 3, 24.99);
+
+-- Create `customer_review` table
+CREATE TABLE `customer_review` (
+  `review_id` int(11) NOT NULL AUTO_INCREMENT,
+  `customer_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `rating` int(11) NOT NULL,
+  `comment` text,
+  PRIMARY KEY (`review_id`),
+  CONSTRAINT `fk_review_customers` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_review_orders` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_review_products` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Create the customer_review table
+DROP TABLE IF EXISTS `customer_review`;
+
+CREATE TABLE `customer_review` (
+  `review_id` int(11) NOT NULL AUTO_INCREMENT,
+  `customer_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `rating` int(11) NOT NULL,
+  `comment` text,
+  PRIMARY KEY (`review_id`),
+  CONSTRAINT `fk_review_customers` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_review_orders` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_review_products` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Insert data into the customer_review table
+INSERT INTO `customer_review` (`customer_id`, `order_id`, `product_id`, `rating`, `comment`)
+VALUES
+  (1, 1, 1, 5, 'Great product!'),
+  (2, 2, 2, 4, 'Good quality, fast shipping'),
+  (3, 3, 3, 5, 'Excellent shoes, very comfortable'),
+  (4, 4, 4, 3, 'Nice jacket, but the size is a bit big'),
+  (5, 5, 5, 5, 'Love the hoodie!'),
+  (1, 1, 6, 4, 'Nice fit, good quality'),
+  (2, 2, 7, 5, 'Perfect shorts for running');
+
+-- Calculate and update order_total in the orders table
+UPDATE orders o
+SET order_total = (
+  SELECT SUM(oi.quantity * psi.unit_price)
+  FROM order_items oi
+  JOIN products_in_stock psi ON oi.products_in_stock_id = psi.stock_id
+  WHERE oi.order_id = o.order_id
+)
+WHERE o.order_id IS NOT NULL;
+
+
+-- Display the orders table
+SELECT * FROM orders;
+
+
 
 
 
